a763b9df412d2262f277d4f68a99a60953c95848
sibi balu <sibibaluk642@gmail.com>
2020-08-16 11:19:14 +0530

    Initial Commit find files based on time and extension

--- findfiles.py
+++ findfiles.py
@@ -2,7 +2,6 @@ import os
 import time
 
 now = time.ctime()
-filecompare = time.time() - 7 * 86400
 print(now)
 Directory = input("Please enter the path : ")
 keyword = input("Please enter file extension : ")
@@ -12,6 +11,6 @@ if not os.path.exists(Directory):
     Directory = "."
 for files, dirs, base in os.walk(Directory):
     for file in base:
-        #filestamp = os.stat(os.path.join(Directory, file)).st_mtime
-        if file.endswith(keyword): #  and filestamp < filecompare:
-            print(file)
+        compare = time.time() - 7 * 86400
+        if file.endswith(keyword) and os.stat(os.path.join(Directory, file)).st_mtime < compare:
+            print(os.path.join(Directory, file))
